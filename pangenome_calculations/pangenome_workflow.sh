c7d17ad1b80295c54820c33e824057a1c4032eec
koper86 <piotrkoper86@gmail.com>
2021-08-05 13:31:22 +0200

    pangenome workflow update

--- pangenome_calculations/pangenome_workflow.sh
+++ pangenome_calculations/pangenome_workflow.sh
@@ -1,4 +1,32 @@
 # all the analysis were conducted using conda env 'bioinfo'. .yml env file is present
 
 # running split_and_extract_gff.py script for prokka annotatad genomes to get separate replicon files
-nohup python split_and_extract_gff.py prokka_results/ ././mutipartite_vibrio_pseudo.xls split_gff.log &
+nohup python split_and_extract_gff.py prokka_results/ ./mutipartite_vibrio_pseudo.xls split_gff.log &
+
+# move chromosome, chromid, megaplasmid gff files to separate folders
+mkdir chromosome_gff
+mkdir chromid_gff
+mkdir megaplasmid_gff
+
+find . -type f -iname '*chromosome*.gff' -exec cp {} pangenome_calculations/chromosome_gff/ \;
+find . -type f -iname '*chromid*.gff' -exec cp {} pangenome_calculations/chromid_gff/ \;
+find . -type f -iname '*megaplasmid*.gff' -exec cp {} pangenome_calculations/megaplasmid_gff/ \;
+
+# species level pangenomes for entire genomes
+roary
+
+# genus level pangenomes for the entire genomes
+
+#pangenomy dla replikonów liczone są na poziomie gatunku w pracy diCenzo!!
+
+# chromosome level pangenome (all genomes)
+roary -e -n -i 90 -g 100000 -f pangenome_calculations/chromosome_result pangenome_calculations/chromosome_gff/*.gff
+
+# chromid pangenome (all genomes)
+
+
+# megaplasmid pangenome (all genomes)
+
+
+# second megaplasmid pangenome
+
--- prokka_annotation/prokka_workflow.sh
+++ prokka_annotation/prokka_workflow.sh
@@ -8,6 +8,7 @@ tar -zxvf Vibrionales.tar.gz
 # running prokka_annotation.py, input_data and prokka_results parameters are relative or absolute paths
 nohup python prokka_annotation.py  Multipartite/ prokka_results > prokka_annotation.log &
 
-
+# checking script running
+cat prokka_annotation.log | grep successfully | wc -l
 
 
